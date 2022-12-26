6079905d868b88c158e35a74cb3a72bb36ba598a
Pobarul-islam <pobarul3@gmail.com>
2022-12-26 14:56:12 +0600

    product item styled

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -5,19 +5,15 @@ export default function ProductItem({ product }) {
   return (
     <div className="card">
       <Link href={`/product/${product.slug}`}>
-    
-          <img
-            src={product.image}
-            alt={product.name}
-            className="rounded shadow"
-          />
-      
+        <img
+          src={product.image}
+          alt={product.name}
+          className="rounded shadow"
+        />
       </Link>
       <div className="flex flex-col items-center justify-center p-5">
         <Link href={`/product/${product.slug}`}>
-         
-            <h2 className="text-lg">{product.name}</h2>
-         
+          <h2 className="text-lg">{product.name}</h2>
         </Link>
         <p className="mb-2">{product.brand}</p>
         <p>${product.price}</p>
@@ -25,7 +21,6 @@ export default function ProductItem({ product }) {
           Add to cart
         </button>
       </div>
-      
     </div>
   );
 }
