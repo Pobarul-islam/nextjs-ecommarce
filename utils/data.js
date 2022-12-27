bf527f61ce005f8e11469b3f4f0c14794c182cac
Pobarul-islam <pobarul3@gmail.com>
2022-12-27 17:35:43 +0600

    mongodb connected and user api added

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users: [
+    {
+      name: 'John',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jane',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  
+  ],
   products: [
     {
       name: 'Free Shirt',
@@ -77,5 +93,3 @@ const data = {
 };
 
 export default data;
-
-
