7e11c6253d2d9e808d902e19114b27b6401bd999
Pobarul-islam <pobarul3@gmail.com>
2022-12-27 12:59:05 +0600

    cookies error solved

--- components/Layouts.js
+++ components/Layouts.js
@@ -1,12 +1,15 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Store } from '../utils/Store';
 
 export default function Layouts({ children, title }) {
     const {state, dispatch} = useContext(Store)
     const {cart} = state;
-    
+    const [cartItemsCount, setCartItemsCount] = useState(0);
+    useEffect(()=>{
+      setCartItemsCount(cart.cartItems.reduce((a, c)=>a + c.quantity, 0))
+    },[cart.cartItems]);
   return (
     <>
       <Head>
@@ -22,12 +25,13 @@ export default function Layouts({ children, title }) {
             </Link>
             <div>
               <Link href="/cart">
-                <span className="p-2">Cart
-                {cart.cartItems.length > 0 && (
-                    <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white'>
-                        {cart.cartItems.reduce((a, c)=>a + c.quantity, 0)}
+                <span className="p-2">
+                  Cart
+                  {cartItemsCount > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cartItemsCount}
                     </span>
-                )}
+                  )}
                 </span>
               </Link>
               <Link href="/login">
