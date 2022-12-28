0b1a60f18a77fe8e1229bfce794ffdc0d585e684
Pobarul-islam <pobarul3@gmail.com>
2022-12-28 21:46:41 +0600

    login and logout solved

---
+++ components/DropdownLink.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import React from 'react'
+
+function DropdownLink(props) {
+    let {href, children, ...rest} = props;
+  return (
+ 
+      <Link href={href}>
+        <span {...rest}>{children}</span>
+      </Link>
+   
+  );
+}
+
+export default DropdownLink
