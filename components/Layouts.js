4c5b9edec5f4f7a2bb2a1afda34e390df13698a5
Pobarul-islam <pobarul3@gmail.com>
2022-12-29 01:06:26 +0600

    navbar updated

--- components/Layouts.js
+++ components/Layouts.js
@@ -1,5 +1,3 @@
-
-import { Menu } from '@headlessui/react';
 import Cookies from 'js-cookie';
 import { signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
@@ -8,7 +6,7 @@ import { useContext, useEffect, useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Store } from '../utils/Store';
-import DropdownLink from './DropdownLink';
+
 
 export default function Layouts({ children, title }) {
   const { status, data: session } = useSession();
@@ -19,13 +17,11 @@ export default function Layouts({ children, title }) {
     setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
   }, [cart.cartItems]);
 
-
-  const logoutClickHandler = ()=>{
+  const logoutClickHandler = () => {
     Cookies.remove('cart');
-    dispatch({type: 'CART_RESET'});
-    signOut({callbackUrl: '/login'});
-  }
-
+    dispatch({ type: 'CART_RESET' });
+    signOut({ callbackUrl: '/login' });
+  };
 
   return (
     <>
@@ -34,68 +30,127 @@ export default function Layouts({ children, title }) {
         <meta name="description" content="Ecommarce website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <ToastContainer position="bottom-center" limit={1}></ToastContainer>
-
-      <div className="flex min-h-screen flex-col justify-between">
-        <header>
-          <nav className="flex h-12 justify-between shadow-md items-center px-4">
-            <Link href="/">
-              <p className="text-lg font-bold"> E-market</p>
-            </Link>
-            <div>
-              <Link href="/cart">
-                <span className="p-2">
-                  Cart
-                  {cartItemsCount > 0 && (
-                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
-                      {cartItemsCount}
-                    </span>
-                  )}
-                </span>
-              </Link>
-
-              {status === 'loading' ? (
-                'Loading'
-              ) : session?.user ? (
-                <Menu as="div" className="relative inline-block">
-                  <Menu.Button className="text-blue-600">
-                    {session.user.name}
-                  </Menu.Button>
-                  <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white  shadow-lg ">
-                    <Menu.Item>
-                      <DropdownLink className="dropdown-link" href="/profile">
-                        Profile
-                      </DropdownLink>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <DropdownLink
-                        className="dropdown-link"
-                        href="/order-history"
-                      >
-                        Order History
-                      </DropdownLink>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <a
-                        className="dropdown-link"
-                        href="#"
-                        onClick={logoutClickHandler}
-                      >
-                        Logout
-                      </a>
-                    </Menu.Item>
-                  </Menu.Items>
-                </Menu>
-              ) : (
-                <Link href="/login">
-                  <span className="p-2">Login</span>
-                </Link>
-              )}
+      <header>
+        <nav className="navbar shadow bg-base-100">
+          <div className="navbar">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost btn-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h7"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Homepage</a>
+                </li>
+                <li>
+                  <a>Portfolio</a>
+                </li>
+                <li>
+                  <a>About</a>
+                </li>
+              </ul>
             </div>
-          </nav>
-        </header>
-
+            <Link className="font-bold" href="/">
+              E-market
+            </Link>
+          </div>
+          <div className="flex-none">
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle">
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+                  <Link
+                    href=""
+                    className="badge badge-sm bg-blue-600 p-2 indicator-item"
+                  >
+                    {cartItemsCount > 0 && (
+                      <span className="">{cartItemsCount}</span>
+                    )}
+                  </Link>
+                </div>
+              </label>
+              <div
+                tabIndex={0}
+                className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+              >
+                <div className="card-body">
+                  <span className="font-bold text-lg">
+                    {cartItemsCount} Items
+                  </span>
+                  <span className="text-info">Subtotal: $999</span>
+                  <div className="card-actions">
+                    <Link href='/cart' className="btn btn-primary btn-block">
+                      View cart
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src="https://placeimg.com/80/80/people" />
+                </div>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  {status === 'loading' ? (
+                    'Loading'
+                  ) : session?.user ? (
+                    <a>{session.user.name}</a>
+                  ) : (
+                    <Link href="/login">
+                      <span>Login</span>
+                    </Link>
+                  )}
+                  <span className="justify-between">Profile</span>
+                </li>
+                <li>
+                  <Link href="/order-history">Order History</Link>
+                </li>
+                <li>
+                  <Link href="#" onClick={logoutClickHandler}>
+                    Logout
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <div className="flex min-h-screen flex-col justify-between">
         <main className="container m-auto mt-4 px-4">{children}</main>
 
         <footer className="flex h-10 justify-center items-center shadow-inner">
---
+++ pages/shipping.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShippinScreen() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShippinScreen;
